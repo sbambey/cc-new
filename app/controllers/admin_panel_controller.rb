76a49a581d3721465f412f7cada962ae214935f2
Simon Bambey <sbambey.lp@gmail.com>
2015-02-02 14:11:50 -0800

    Updates admin panel to only show most recent consistency

--- app/controllers/admin_panel_controller.rb
+++ app/controllers/admin_panel_controller.rb
@@ -2,7 +2,8 @@ class AdminPanelController < ApplicationController
 	before_action :authenticate_user!, :redirect_unless_admin
 
 	def home
-		@node_sets = ScrapeNodeSet.all.order(id: :desc).limit(90)
+		#@node_sets = ScrapeNodeSet.all.order(id: :desc).limit(90)
+		@node_sets = ScrapeNodeSet.where("created_at >= ?", ScrapeNodeSet.where(airline_id: 2).last.created_at)
 		#@admin_notices = AdminNotice.all.order(id: :desc).limit(30)
 	end
 
