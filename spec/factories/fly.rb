bc904fd9e3bf9101351dd04e5f83784745a93728
Simon Bambey <sbambey.lp@gmail.com>
2015-02-03 00:31:11 -0800

    Adjusts udpated_at on admin panel user_management

--- spec/factories/fly.rb
+++ spec/factories/fly.rb
@@ -33,9 +33,13 @@ FactoryGirl.define do
     website "http://test.com"
     equipment "Airbus A320"
     base "Sandyland"
-    flight_time { {"total_time" => "100", "total_pic_time" => "10", "multi_time" => "10", "multi_pic_time" => "10", 
-	  	"turbine_time" => "10", "turbine_pic_time" => "10", "turbofan_time" => "40", "turbofan_pic_time" => "10",
-	  	"night_time" => "0", "instrument_time" => "0", "x_country_time" => "0" } }
+    flight_time { {"total_time" => "0", "total_pic_time" => "0", "multi_time" => "0", "multi_pic_time" => "0", 
+	  	"turbine_time" => "0", "turbine_pic_time" => "0", "turbojet_time" => "0", "turbojet_pic_time" => "0",
+	  	"turboprop_time" => "0", "turboprop_pic_time" => "0",
+	  	"night_time" => "0", "night_pic_time" => "0",
+	  	"x_country_time" => "0", "x_country_pic_time" => "0",
+	  	"instrument_time" => "0", "actual_instrument" => "0",
+	  	"single_float_time" => "0", "multi_float_time" => "0" } }
 	  rating RATINGS[:atpl_unrestricted]
 	  medical_license MEDICAL_LICENSES[:one]
 	  added_requirements { ["some requirement", "some other requirement"] }
@@ -43,9 +47,14 @@ FactoryGirl.define do
 	  posting_date_as_date Date.yesterday
 	  posting_expiry_as_string "10th of Feb 2080"
 	  posting_expiry_as_date Date.today
+	  content_selector ".someCLass"
+	  flight_experience { { "atp_qualifications" => "0", "atp_written" => "0", "multi_engine" => "0", "instrument" => "0",
+	  	"float" => "0" } }
 	  no_track true
 	  no_match false
 	  airline
+	  deleted_at nil
+	  updated false
   end
 
   factory :fly_without_requirements, class: Fly do
