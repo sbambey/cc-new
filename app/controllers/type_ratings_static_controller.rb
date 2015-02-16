8e99edeef2e958a8939c0dec1ee38ec6767965e7
Simon Bambey <sbambey.lp@gmail.com>
2015-02-16 13:04:32 -0800

    Fixes header to show active Blog

--- app/controllers/type_ratings_static_controller.rb
+++
@@ -1,9 +0,0 @@
-class TypeRatingsStaticController < ApplicationController
-	before_action :authenticate_user!
-	before_action :redirect_unless_admin, except: [:index]
-
-	def index
-		@ratings = TypeRatingStatic.order(:designation).where("designation LIKE ?", "%#{params[:term]}%")
-		render json: @ratings.map(&:designation)
-	end
-end
--- app/helpers/type_ratings_helper.rb
+++
@@ -1,2 +0,0 @@
-module TypeRatingsHelper
-end
