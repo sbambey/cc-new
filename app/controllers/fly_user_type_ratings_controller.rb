b2f3721744c91bfe8c270abcfd62a2a2d83d917f
Simon Bambey <sbambey.lp@gmail.com>
2015-02-18 10:31:48 -0800

    Adds type ratings to views and permits attr

--- app/controllers/fly_user_type_ratings_controller.rb
+++ app/controllers/fly_user_type_ratings_controller.rb
@@ -1,12 +1,3 @@
 class FlyUserTypeRatingsController < ApplicationController
 
-	def create
-		@fly_user_type_rating = FlyUserTypeRating.create(fly_user_type_rating_params)
-	end
-
-	private
-
-	def fly_user_type_rating_params
-		params.require(:fly_user_type_rating).permit(:type_rating_id, :fly_id, :user_id, :issuance, :time_in_type, :current, :pic)
-  end
 end
