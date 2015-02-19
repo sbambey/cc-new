c79ce479d5526a1d203d5c82e7dff2968f8f744d
Simon Bambey <sbambey.lp@gmail.com>
2015-02-18 18:11:40 -0800

    Mailer fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,15 +84,15 @@ Rails.application.configure do
     }
   }
 
+  #config.action_mailer.default_url_options = { host: "smtp.sendgrid.net" }
+  config.action_mailer.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
-    :address        => 'smtp.sendgrid.net',
-    :port           => '587',
+    :user_name => ENV["SENDGRID_USERNAME"],
+    :password => ENV["SENDGRID_PASSWORD"],
+    :domain => 'www.flightcrew.io',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
     :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
-  ActionMailer::Base.delivery_method = :smtp
-  Rails.application.routes.default_url_options[:host] = 'www.flightcrew.io'
 end
